625a6c47c9100e4fe9f34f0a1b51a3d4726518a8
annisayusoff <93319358+annisayusoff@users.noreply.github.com>
2024-05-25 02:29:31 +0800

    code for data transformation

---
+++ transformation/3.constructor_standings.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date", "2021-03-28")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import when, col, sum, desc, rank, count
+from pyspark.sql.window import Window
+
+# COMMAND ----------
+
+#call the required parquet file 
+race_results_df = spark.read.format("delta").load(f"{presentation_folder_path}/race_results") \
+                    .filter(f"file_date = '{v_file_date}'") 
+                    
+race_year_list = df_column_to_list(race_results_df, 'race_year')
+
+race_results_df = spark.read.format("delta").load(f"{presentation_folder_path}/race_results") \
+.filter(col("race_year").isin(race_year_list))
+
+# COMMAND ----------
+
+#perform transformation on the data to achieve the desired results
+constructor_standings_df = race_results_df \
+                            .groupBy("race_year", "team") \
+                            .agg(count(when(col("position") == 1, True)).alias("wins"), 
+                                 sum("points").alias("total_points"))
+
+constructor_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+final_df = constructor_standings_df.withColumn("rank", rank().over(constructor_rank_spec))
+
+# COMMAND ----------
+
+merge_condition = "tgt.team = src.team AND tgt.race_year = src.race_year"
+merge_delta_data(final_df, 'f1_presentation', 'constructor_standings', presentation_folder_path, merge_condition, 'race_year')
